d70adc4d1ebce1e82a81d259f37ea8c18f6b2da3
kabwe <kasomakabwe@gmail.com>
2020-09-08 20:23:15 +0200

    add login, redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+//Authentication Imports
+import {
+  NbAuthComponent,
+  NbLoginComponent,
+  NbRegisterComponent,
+  NbLogoutComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent,
+} from '@nebular/auth';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: '',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'register',
+        component: NbRegisterComponent,
+      },
+      {
+        path: 'logout',
+        component: NbLogoutComponent,
+      },
+      {
+        path: 'request-password',
+        component: NbRequestPasswordComponent,
+      },
+      {
+        path: 'reset-password',
+        component: NbResetPasswordComponent,
+      },
+    ],
   },
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   { path: 'administration', loadChildren: () => import('./administration/administration.module').then(m => m.AdministrationModule) },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NbIconModule, NbLayoutModule, NbMenuModule, NbSidebarModule, NbThemeModule} from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NbPasswordAuthStrategy, NbAuthModule, NbAuthJWTToken } from '@nebular/auth';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,45 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     NbIconModule,
     NbSidebarModule.forRoot(), 
     NbMenuModule.forRoot(),
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    HttpClientModule,
+    NbAuthModule.forRoot({
+      strategies: [
+        NbPasswordAuthStrategy.setup({
+          name: 'email',
+          token: {
+            class: NbAuthJWTToken,
+            key: 'token', // this parameter tells where to look for the token
+          },
+          baseEndpoint: 'http://localhost/ePassAPI',
+          login: {
+            endpoint: '/sign-in.php',
+            method: 'post',
+            redirect: {
+              success: 'dashboard',
+              failure: null, // stay on the same page
+            },
+          },
+          register: {
+            endpoint: '/sign-up',
+            method: 'post',
+          },
+          logout: {
+            endpoint: '/sign-out',
+            method: 'post',
+          },
+          requestPass: {
+            endpoint: '/request-pass',
+            method: 'post',
+          },
+          resetPass: {
+            endpoint: '/reset-pass',
+            method: 'post',
+          },
+        }),
+      ],
+      forms: {},
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
