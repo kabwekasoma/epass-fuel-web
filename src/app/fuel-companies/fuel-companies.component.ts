687d9100c68605a2382487c98202ee8aff566a86
kabwe <kasomakabwe@gmail.com>
2020-09-09 02:25:16 +0200

    get companies

--- src/app/fuel-companies/fuel-companies.component.ts
+++ src/app/fuel-companies/fuel-companies.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 @Component({
   selector: 'app-fuel-companies',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuelCompaniesComponent implements OnInit {
 
-  constructor() { }
+  settings = {
+    columns: {
+      companyName: {
+        title: "Company Name"
+      },
+    }
+  };
+  data;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    // Simple GET request with response type <any>
+    this.http.get("http://localhost/ePassAPI/fuel-company.php").subscribe(user => (this.data = user));
   }
 
 }
--- src/app/fuel-companies/fuel-companies.module.ts
+++ src/app/fuel-companies/fuel-companies.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FuelCompaniesRoutingModule } from './fuel-companies-routing.module';
 import { FuelCompaniesComponent } from './fuel-companies.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { Ng2CompleterModule } from "@akveo/ng2-completer";
 
 
 @NgModule({
   declarations: [FuelCompaniesComponent],
   imports: [
     CommonModule,
-    FuelCompaniesRoutingModule
+    HttpClientModule,
+    FuelCompaniesRoutingModule,
+    Ng2SmartTableModule,
   ]
 })
 export class FuelCompaniesModule { }
