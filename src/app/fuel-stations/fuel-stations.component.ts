f9b9a7509d5da255880886414d97d402b472d2c9
kabwe <kasomakabwe@gmail.com>
2020-09-14 01:00:16 +0200

    CRUD Fuel Stations

--- src/app/fuel-stations/fuel-stations.component.ts
+++ src/app/fuel-stations/fuel-stations.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { NbAuthJWTToken, NbAuthService } from '@nebular/auth';
+
+
+import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
+
 
 @Component({
   selector: 'app-fuel-stations',
@@ -7,9 +13,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuelStationsComponent implements OnInit {
 
-  constructor() { }
+  data;
+  source: LocalDataSource;
+  users = {}; //for logged in user
+
+  settings = {
+    add: {
+      confirmCreate: true,
+         },
+    edit: {
+      confirmSave: true,
+        },
+    delete: {
+      confirmDelete: true,
+        },
+    columns: {
+      stationId: {
+        title: "ID",
+        filter: false,
+        editable: false
+      },
+      stationName: {
+        title: "Station Name",
+        filter: false
+      },
+    }
+  };
+
+  constructor(private http: HttpClient, private authService: NbAuthService) {
+    this.authService.onTokenChange()
+      .subscribe((token: NbAuthJWTToken) => {
+
+        if (token.isValid()) {
+          this.users = token.getPayload(); // here we receive a payload from the token and assigns it to our `user` variable 
+          console.log(this.users[0]);
+        }
+
+      });
+    this.source = new LocalDataSource(this.data);
+   }
 
   ngOnInit(): void {
+    // Simple GET request with response type <any> 
+    this.http.post("http://localhost/ePassAPI/v1/fuel-stations.php", this.users[0]).subscribe(user => (this.data = user));
+    //this.http.get("http://test.epassapi.dczambia.com/v1/fuel-stations.php").subscribe(user => (this.data = user));
+  }
+
+  onAddCall(event){
+    event.confirm.resolve(event.newData);
+            event.newData.source = "add"; 
+            console.log(event.newData); //this contains the new edited data
+        //post request
+    const req = this.http.post('http://localhost/ePassAPI/v1/fuel-stations.php', event.newData);
+    //const req = this.http.post('http://test.epassapi.dczambia.com/v1/fuel-stations.php', event.newData);
+     // 0 requests made - .subscribe() not called.
+    req.subscribe();
+   // 1 request made.
+  }
+  onEditCall(event){
+    event.confirm.resolve(event.newData);
+            event.newData.source = "edit"; 
+            console.log(event.newData); //this contains the new edited data
+        //post request
+    const req = this.http.post('http://localhost/ePassAPI/v1/fuel-stations.php', event.newData);
+    //const req = this.http.post('http://test.epassapi.dczambia.com/v1/fuel-stations.php', event.newData);
+     // 0 requests made - .subscribe() not called.
+    req.subscribe();
+   // 1 request made.
+  }
+  onDeleteCall(event){
+    event.confirm.resolve(event.data);
+            event.data.source = "delete"; 
+            console.log(event.data); //this contains the new edited data
+        //post request
+    const req = this.http.post('http://localhost/ePassAPI/v1/fuel-stations.php', event.data);
+    //const req = this.http.post('http://test.epassapi.dczambia.com/v1/fuel-stations.php', event.data);
+     // 0 requests made - .subscribe() not called.
+    req.subscribe();
+   // 1 request made.
+  }
+
+  onSearch(query: string = '') {
+    this.source.setFilter([
+      // fields we want to include in the search
+      {
+        field: 'stationId',
+        search: query
+      },
+      {
+        field: 'stationName',
+        search: query
+      },
+    ], false); 
+    // second parameter specifying whether to perform 'AND' or 'OR' search 
+    // (meaning all columns should contain search query or at least one)
+    // 'AND' by default, so changing to 'OR' by setting false here
   }
 
 }
--- src/app/fuel-stations/fuel-stations.module.ts
+++ src/app/fuel-stations/fuel-stations.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FuelStationsRoutingModule } from './fuel-stations-routing.module';
 import { FuelStationsComponent } from './fuel-stations.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { Ng2CompleterModule } from "@akveo/ng2-completer";
+import { NbCardModule } from '@nebular/theme';
 
 
 @NgModule({
   declarations: [FuelStationsComponent],
   imports: [
     CommonModule,
-    FuelStationsRoutingModule
+    FuelStationsRoutingModule,
+    HttpClientModule,
+    Ng2SmartTableModule,
+    NbCardModule,
   ]
 })
 export class FuelStationsModule { }
