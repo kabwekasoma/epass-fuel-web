524962372dc66e67bb5085f56ab5353c54157d2a
kabwe <kasomakabwe@gmail.com>
2020-09-13 13:14:51 +0200

    user specific menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,20 +12,9 @@ export class AppComponent {
   title = 'ePass';
 
   user = {}; //for logged in user
+  userLevel = "";
 
-  constructor(private readonly sidebarService: NbSidebarService, private authService: NbAuthService) {
-    this.authService.onTokenChange()
-      .subscribe((token: NbAuthJWTToken) => {
-
-        if (token.isValid()) {
-          this.user = token.getPayload(); // here we receive a payload from the token and assigns it to our `user` variable 
-          //console.log(this.user[0].email);
-        }
-
-      });
-  }
-
-  items: NbMenuItem[] = [
+  itemsBookNowAdmin: NbMenuItem[] = [
     {
       title: 'Dashboard',
       icon: 'home-outline',
@@ -42,6 +31,22 @@ export class AppComponent {
           icon: 'chevron-right-outline',
           link: '/fuel-companies',
         },
+      ],
+    }
+  ];
+
+  itemsFuelCompanyAdmin: NbMenuItem[] = [
+    {
+      title: 'Dashboard',
+      icon: 'home-outline',
+      link: '/dashboard',
+      home: true
+    },
+    {
+      title: 'Administration',
+      icon: 'people-outline',
+      link: '/administration',
+      children: [
         {
           title: 'Fuel Stations',
           icon: 'chevron-right-outline',
@@ -56,8 +61,29 @@ export class AppComponent {
     }
   ];
 
+  constructor(private readonly sidebarService: NbSidebarService, private authService: NbAuthService) {
+    this.authService.onTokenChange()
+      .subscribe((token: NbAuthJWTToken) => {
+
+        if (token.isValid()) {
+          this.user = token.getPayload(); // here we receive a payload from the token and assigns it to our `user` variable 
+          //console.log(this.user[0].email);
+
+          //User Specific Menu
+          if(this.user[0].level == '2'){
+            this.userLevel = "booknow-admin";
+          }
+          if(this.user[0].level == '4'){
+            this.userLevel = "fuel-company-admin";
+          }
+        }
+
+      });
+  }
+
   toggleSidebar(): boolean {
     this.sidebarService.toggle();
     return false;
   }
+  
 }
